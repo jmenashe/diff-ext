0e9620b2064798c11e6b2ab9b46bb4c88b571328
serg_z <serg_z@c5e57457-970d-0410-8758-e1b5818465f3>
2008-11-19 14:20:19 +0000

    port to kde4 & cmake; local install to $KDEHOME directory works, global install have to be fixed

--- kdiff-ext/setup/src/diff.h
+++ kdiff-ext/setup/src/diff.h
@@ -1,27 +1,26 @@
-
 #ifndef DIFF_H
 #define DIFF_H
 
+#include <kconfigdialog.h>
+
 #include "ui_base.h"
 
-class DIFF : public QWidget, Ui_BASE
-{
+class DIFF : public QWidget, Ui_BASE {
   Q_OBJECT
 
-public:
-  DIFF(QWidget* parent = 0);
-  ~DIFF();
-  /*$PUBLIC_FUNCTIONS$*/
+  public:
+    DIFF(QWidget* parent, QString prefix);
+    ~DIFF();
+    /*$PUBLIC_FUNCTIONS$*/
 
-public slots:
-  /*$PUBLIC_SLOTS$*/
+  public slots:
+    void browse();
 
-protected:
-  /*$PROTECTED_FUNCTIONS$*/
+  protected:
+    /*$PROTECTED_FUNCTIONS$*/
 
-protected slots:
-  /*$PROTECTED_SLOTS$*/
-
+  protected slots:
+    /*$PROTECTED_SLOTS$*/
 };
 
 #endif
